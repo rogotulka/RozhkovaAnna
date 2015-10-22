a80aaa8fa1e85f851d309563949316ccfd09d603
Ann Rozhkova <a.rozhkova@nemotelecom.co.uk>
2015-10-22 16:31:50 +0300

    add test for main activity

---
+++ app/src/androidTest/java/org/rogotulka/rozhkovaanna/MainActivityTest.java
@@ -0,0 +1,9 @@
+package org.rogotulka.rozhkovaanna;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTest(Class<MainActivity> activityClass) {
+        super(activityClass);
+    }
+}
